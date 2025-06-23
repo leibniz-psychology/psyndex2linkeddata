c3000a1c26867043518e87f93da01b2b0b6f905b
Tina Trillitzsch <ttr@leibniz-psychology.org>
2025-06-23 14:55:46 +0200

    feat: add test code for rplics

---
+++ testing/RPLIC/test_rplic.py
@@ -0,0 +1,132 @@
+rplics = (
+    "Testeintrag, wieder loeschen",
+    "https://osf.io/kv65n/",
+    "Timmer, K., Calabria, M., Branzi, F. M., Baus, C., &amp; Costa, A. (2018). On the reliability of switching costs  across time and domains. Frontiers in Psychology, 9, 1032.",
+    "Stumbrys, T., &amp; Erlacher, D. (2014). The science of lucid dream induction. In R. Hurd, &amp; K. Bulkeley (Eds.). Lucid dreaming: New perspectives on consciousness in sleep (pp. 77^DDS96). Westport: Praeger.",
+    "Mattler, U., &amp; Fendrich, R. (2010). Consciousness mediated by neural transition states: How invisiblyrapid motions can become visible.Consciousness and Cognition,19, 172^DDS185. https://doi.org/10.1016/j.concog.2009.12.015",
+    "Dreßing H, Kuehner C, Gass P: Lifetime prevalence and impact of stalking in a European population: epidemiological data from a middlesized German city. Br J Psychiat 2005; 187: 168^DDS72",
+    "Daw ND, O^D&gt;'Doherty JP, Dayan P, Seymour B, Dolan RJ. 2006. Cortical substrates for exploratory decisions inhumans.Nature441:876^DDS879.DOI: https://doi.org/10.1038/nature04766,PMID: 16778890",
+    "Kahneman, D., &amp; Tversky. A. (1979). Prospect theory: An analysis of decision under risk. Econometrica, 47(2), 263-292.",
+    "Saalbach, H., Eckstein, D., Andri, N., Hobi, R., &amp; Grabner, R. H. (2013). When language of instruction and language of application differ: Cognitive costs of bilingual mathematics learning. Learning and Instruction, 26, 36-44.",
+    "Grabner, R. H., Saalbach, H., &amp; Eckstein, D. (2012). Language‐Switching Costs in Bilingual Mathematics Learning. Mind, Brain, and Education, 6(3), 147-155.",
+    "Eskine, K. J., Kacinik, N. A., &amp; Prinz, J. J. (2011). A bad taste in the mouth: Gustatory disgustinfluences moral judgment.Psychological Science,22(3), 295^DDS299.",
+    "Head, D., &amp; Isom, M. (2010). Age effects on wayfinding and route learn-ing skills.Behavioural Brain Research,209,49^DDS58.",
+    "Rütsche, B., Hauser, T. U., Jäncke, L., and Grabner, R. H. (2015). Whenproblem size matters: differential effects of brain stimulation on arithmeticproblem  solving  and  neural  oscillations. PLoS ONE10:e0120665.doi: 10.1371/journal.pone.0120665",
+    "https://doi.org/10.1101/470435",
+    "Armstrong, T., Bilsky, S. A., Zhao, M., &amp; Olatunji, B. O. (2013). Dwellingon potential threat cues: An eye movement marker for combat-relatedPTSD.Depression and Anxiety, 30,497^DDS502.",
+    "Kugler, K. G., Reif, J. A. M., Kaschner, T., &amp; Brodbeck, F. C. (2018). Gender differences in the initiation of negoti-ations: A meta-analysis.Psychological Bulletin,144, 198^DDS222.",
+    "Tietze, W., Becker-Stoll, F., Bensel, J., Eckhardt, A. G., Haug-Schnabel, G., Kalicki, B.,. . . &amp; Leyendecker, B. (2013). Nationale Untersuchung zur Bildung, Betreuung undErziehung in der frühen Kindheit (NUBBEK). Weimar: verlag das netz.",
+    "Korb, F. M., Jiang, J., King, J. A., &amp; Egner, T. (2017). Hierarchicallyorganized medial frontal cortex-basal ganglia loops selectively controltask- and response-selection.Journal of Neuroscience: The Official Journal of the Society for Neuroscience, 37,7893^DDS7905.http://dx.doi.org/10.1523/JNEUROSCI.3289-16.2017",
+    "Langguth,  B. et al. Tinnitus severity, depression, and the big five personality traits. In Langguth, B., Hajak, G., Kleinjung, T., Cacace, A. &amp; Møller, A. R. (eds.) Progress in Brain Research, vol. 166 of Tinnitus: Pathophysiology and Treatment, 221^DDS225.",
+    "Schmitz, J., Krämer, M., Blechert, J., &amp; Tuschen-Caffier, B. (2010). Post-event processing in children with social phobia.Journal ofAbnormal Child Psychology, 38(7), 911^DDS919.",
+    "Gamble, T., &amp; Walker, I. (2016). Wearing a bicycle helmet can increase risk taking and sensation seeking in adults. Psychological Science, 27(2),  289^DDS294.  https ://doi.org/10.1177/09567 97615 620784",
+    'Sprenger,  L.,  Becker,  K.,  Heinzel-Gutenbrunner,  M.,  Mingebach,  T.,  Otterbach,  S.,  Peters,  M.,  &amp;  Kamp-Becker,  I.  (2015).  Is  the  ^D&lt;,Stepping  Stones/Triple  P^D&gt;"-parenting  program  a  reason-able, additional intervention in the treatment of children with an autism spectrum disorder? Kindheit und Entwicklung, 24, 28^DDS36',
+    "Hagan  CC,  Graham  JM,  Tait  R,  et  al.  Adolescents  with  current  major  depressive  disorder  show  dissimilar  patterns  of  age-related  differences in ACC and thalamus. Neuroimage Clin 2015;7:391-9.",
+    "Jaworska  N,  Yucel  K,  Courtright  A,  et  al.  Subgenual  anterior  cin-gulate  cortex  and  hippocampal  volumes  in  depressed  youth:  the  role of comorbidity and age. J Affect Disord 2016;190:726-32.",
+    "Tran, U. S., Glück, T. M., &amp; Nader, I. W. (2013). Investigating the FiveFacet Mindfulness Questionnaire (FFMQ): construction of a shortform and evidence of a two-factor higher order structure of mind-fulness.Journal of Clinical Psychology, 69,951^DDS965.https://doi.org/10.1002/jclp.21996.",
+    " Rand, D. G., Greene, J. D., &amp; Nowak, M. A. (2012). Spontaneous giving and calculated greed. Nature, 489, 427^DDS430",
+    "Borota, D., Murray, E., Keceli, G., Chang, A., Watabe, J. M., Ly, M., . . . Yassa, M. A. 535 (2014). Post-study caffeine administration enhances memory consolidation in humans. 536 Nature Neuroscience, 17 (2), 201^DDS203 |d 10.1038/nn.3623",
+    "Schneider, D., Bayliss, A. P., Becker, S. I., &amp; Dux, P. E. (2012). Eye movements reveal sustained implicit processing of others' mental states. Journal of Experimental Psychology: General, 141, 433-438. |d DOI: 10.1037/a0025458",
+    "validating the pro-environmental behavior task |u https://osf.io/x2qh3/",
+    "|u https://osf.io/x2qh3/",
+    "Vollmer, Heinz C., Domma-Reichart, Julia (2016). Ergebnisorientierte Klassifikation Alkoholabhängiger zu Beginn einer stationären Behandlung. Sucht, 62 (2), 65-72. |d 10.1024/0939-5911/a000412 |f 0311238",
+    "Dale, R., Kehoe, C., & Spivey, M. J. (2007). Graded motor responses inthe time course of categorizing atypical exemplars.Memory &Cognition,35(1), 15^DDS28 |f |u |d",
+    "Höffler, T. N., Prechtl, H., &amp; Nerdel, C. (2010). The influence of visual cog- nitive style when learning from instructional animations and static  pictures. Learning and IndividualDifferences, 20, 479^DDS483. |u https://doi. org/10.1016/j.lindif.2010.03.001",
+    "The universal and automatic association between brightness and positivity. Acta Psychologica, 186, 47^DDS53. |d 10.1016/j.actpsy.2018.04.007",
+    "Attention, Perception &amp; Psychophysics, 69(6), 952957 |f  |u https://link.springer.com/article/10.3758/BF03193932 |d ",
+    "Marks, L. E. (1987). On cross-modal similarity: Auditoryvisual interactions in speeded discrimination. Journal of Experimental Psychology: Human Perception and Performance, 13(3), 384394. https://doi.org/10.1037// 0096-1523.13.3.384 |f  |u https://doi.org/10.1037//0096-1523.13.3.384 |d ",
+    "Henne, P., Perez, K., &amp; McCracken, C. (2023). No evidence that reversibility affects causal judgments in late-preemption cases. Preprint. https:// doi.org/10.31219/osf.io/ky9xn |f  |u https://osf.io/preprints/osf/ky9xn |d ",
+    "Scheer, D., Egener, L., Laubenstein, D. &amp; Melzer, C. (2023). Development and psychometric evalua- tion of an instrument measuring subjective defini- tions of inclusion (FEDI). International Journal of Inclusive Education, 27(4), 472^DDS492. |f  |u https://doi.org/10.1080/13603116.2020.1853256 |d ",
+    "Lüke, T. &amp; Grosche, M. (2018). Konstruktion und Validierung der Professionsunabhängigen Einstel- lungsskala zum Inklusiven Schulsystem (PREIS). Empirische Sonderpädagogik, 10(1), 3^DDS20. |f  |u https://doi.org/10.25656/01:15958 |d ",
+    "Seifried, S. &amp; Heyl, V. (2016). Konstruktion und Validierung eines Einstellungsfragebogens zu nklusion für Lehrkräfte (EFI-L). Empirische Sonderpädagogik, 8(1), 22^DDS35. |f 9007420 |u https://doi.org/10.25656/01:11852 |d ",
+    "https://doi.org/10.1016/j.tate.2021.103346 |f  |u  |d ",
+    "Paul, A. A., Lee, K., &amp; Ashton, M. C. (2022). Who tells prosocial lies? A HEXACO model investigation. Journal of Research in Personality, 98, Article 104232. |f  |u https://doi.org/ 10.1016/j.jrp.2022.104232 |d ",
+    "https://doi.org/10.1086/598971 |f  |u https://academic.oup.com/jcr/article/36/3/406/1846977 |d ",
+    "https://doi.org/10.1111/j.1469-8986.2010.01081.x |f  |u https://onlinelibrary.wiley.com/doi/10.1111/j.1469-8986.2010.01081.x |d ",
+    "Chowdhury, S. M., Jeon, J. Y., &amp; Saha, B. (2017). Gender differences in the giving and taking variants of the dictator game. Southern Economic Journal, 84(2), 474^DDS483 |f  |u https://doi.org/10.1002/soej.12223 |d ",
+    'Kahraman, B., &amp; Knoblich, G. (2000). «Stechen statt Sprechen»: Valenz und Aktivierbarkeit von Ste- reotypen über Türken [^D&gt;"Stabbing instead of talking^D&gt;": Valence and activability of stereotypes about Turks]. Zeitschrift für Sozialpsychologie, 31(1), 31^DDS43. |f 0137014 |u https://doi.org/10.1024//0044-3514.31.1.31 |d ',
+    "Bonefeld, M., &amp; Karst, K. (2020). Döner vs. Schweinebraten ^DDS Stereotype von (angehenden) Lehrkräften über Personen deutscher und türkischer Herkunft im Vergleich [Döner vs. pig roast - stereotypes of teachers (in training) about people of German and Turkish origin in comparison]. In S. Glock &amp; H. Kleen (Eds.), Stereotype in der Schule (pp. 159^DDS190). Springer Fachmedien Wiesbaden. |f  |u https://doi.org/10.1007/978-3-658-27275-3 6 |d ",
+    "https://doi.org/10.1037/pspp0000263 |f  |u  |d ",
+    # "",
+    # "",
+    # "",
+    )
+import modules.mappings as mappings
+import modules.helpers as helpers
+import modules.research_info as research_info
+import html
+
+import modules.abstract as abstract
+
+import modules.instance_source_ids as instance_source_ids
+import modules.instance_sources as instance_sources
+import modules.local_api_lookups as localapi
+import modules.mappings as mappings
+import modules.namespace as ns
+import modules.publication_types as publication_types
+import modules.terms as terms
+
+
+# build nodes for the example rplics:
+# but we need to first generate random work_uris for each rplic, 
+# so we can use them as resource nodes where we attach the relationship
+# nodes. 
+
+# so first, we need to set up the whole graph with the work uris,
+# and in the end, serialize the graph to a file.
+# for this, we need to create a new graph, and then add the work uris as nodes.
+from rdflib import Literal, URIRef, Graph
+from rdflib.namespace import RDF, RDFS, SKOS
+
+import modules.helpers as helpers
+import modules.identifiers as identifiers
+import modules.mappings as mappings
+import modules.namespace as ns
+
+# create a new graph
+graph = Graph()
+graph = Graph(identifier=URIRef("https://w3id.org/zpid/testgraph/"))
+# set the namespace for the graph
+
+# Bind the namespaces to the prefixes we want to see in the output:
+graph.bind("bf", ns.BF)
+graph.bind("bflc", ns.BFLC)
+graph.bind("works", ns.WORKS)
+# records_schema.bind("works", WORKS)
+graph.bind("instances", ns.INSTANCES)
+graph.bind("pxc", ns.PXC)
+graph.bind("pxp", ns.PXP)
+graph.bind("lang", ns.LANG)
+graph.bind("schema", ns.SCHEMA)
+graph.bind("locid", ns.LOCID)
+graph.bind("mads", ns.MADS)
+graph.bind("roles", ns.ROLES)
+graph.bind("relations", ns.RELATIONS)
+graph.bind("genres", ns.GENRES)
+graph.bind("contenttypes", ns.CONTENTTYPES)
+graph.bind("issuances", ns.ISSUANCES)  # issuance types
+graph.bind("pmt", ns.PMT)  # mediacarriers
+graph.bind("licenses", ns.LICENSES)  # licenses
+
+# make a loop to process each citation in rplics and print the resulting object:
+for index,rplic in enumerate(rplics):
+    # clean up DD codes:
+    # rplic = mappings.replace_encodings(rplic)
+    # # clean up encoding errors like &amp; and &lt; using html.unescape
+    # rplic = html.unescape(rplic)
+    # to combine the two:
+    # rplic = mappings.replace_encodings(html.unescape(rplic))
+    # rplic_object = research_info.generate_replication_from_rplic(rplic)
+    # generate a random work uri for the rplic that we attach the nodes to:
+    work_uri = URIRef(
+        "https://w3id.org/zpid/testgraph/works/" + str(index))
+    # add the work uri to the graph:
+    graph.add((work_uri, RDF.type, ns.WORKS.Work))
+    # use research_info.get_rplic_replications to build the nodes
+    # and attach them to the work uri:
+    rplic_object = research_info.get_rplic_replications(
+        work_uri, graph, rplic)
+    # print the rplic object:
+    print(f"RPLIC {index}: {rplic_object}")
+# serialize the graph to a file:
+graph.serialize("test_rplic.ttl", format="turtle")
